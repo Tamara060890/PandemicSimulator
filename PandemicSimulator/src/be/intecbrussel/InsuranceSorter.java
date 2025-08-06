b2adf96f55d7b2dac16b7ecf31cac47a48a26b08
Tamara060890 <toma_tomova@hotmail.com>
2025-08-06 12:56:54 +0200

    Update InsuranceSorter.java

--- PandemicSimulator/src/be/intecbrussel/InsuranceSorter.java
+++ PandemicSimulator/src/be/intecbrussel/InsuranceSorter.java
@@ -3,9 +3,10 @@ package be.intecbrussel;
 import java.util.Comparator;
 
 public class InsuranceSorter implements Comparator<Patient> {
-    // Vergelijkt twee patiënten op verzekeringsstatus
+    // Vergelijk twee patiënten op verzekeringsstatus
     @Override
     public int compare(Patient p1, Patient p2){
         return Boolean.compare(p2.isEnsured(), p1.isEnsured());
     }
 }
+
