b34e1b0927ccb9f9201c8a1dd280cd418cba9591
Tamara060890 <toma_tomova@hotmail.com>
2025-08-06 12:56:15 +0200

    Update AgeSorter.java

--- PandemicSimulator/src/be/intecbrussel/AgeSorter.java
+++ PandemicSimulator/src/be/intecbrussel/AgeSorter.java
@@ -3,9 +3,10 @@ package be.intecbrussel;
 import java.util.Comparator;
 
 public class AgeSorter implements Comparator<Patient> {
-    // Vergelijkt twee patiënten op leeftijd, zodat de oudste eerst komt
+    // Vergelijk twee patiënten op leeftijd, zodat de oudste eerst komt
     @Override
     public int compare(Patient p1, Patient p2){
         return Integer.compare(p2.getAge(), p1.getAge());
     }
 }
+
